c5637ed1d7f84aefb6e413fc5e599d53aed9c294
Majed <boumaged@gmail.com>
2023-02-24 10:13:48 +0400

    Initial Commit

---
+++ Unit8/Unit8App.swift
@@ -0,0 +1,17 @@
+//
+//  Unit8App.swift
+//  Unit8
+//
+//  Created by Majed on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Unit8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
